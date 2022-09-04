74c615250cacde846cb08e0eb5a68575560c6ba3
joelharkes <jlharkes@gmail.com>
2022-09-04 21:01:56 +0200

    Add: reduce(), mapKey(), join() iterator functions

--- src/Iterator/IteratorExtensions.php
+++ src/Iterator/IteratorExtensions.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace DataStructures\Iterator;
 
 use Closure;
+use DataStructures\Map\Map;
 use DataStructures\Set\Set;
 use DataStructures\Stack\Stack;
 
@@ -18,7 +19,8 @@ trait IteratorExtensions
      * @param Closure(TValue, TKey=): bool $filterFn
      * @return FilterIterator<TKey, TValue>
      */
-    public function filter(Closure $filterFn): WrappedIterator{
+    public function filter(Closure $filterFn): WrappedIterator
+    {
         return new FilterIterator($this, $filterFn);
     }
 
@@ -28,9 +30,23 @@ trait IteratorExtensions
      * @return MapIterator<TKey, TMappedValue, TValue>
      * @phpstan-ignore-next-line we know its covariant because we only call the function on this iterator it's content
      */
-    public function map(Closure $mapFn): WrappedIterator {
+    public function map(Closure $mapFn): WrappedIterator
+    {
         /** @var MapIterator<TKey, TMappedValue, TValue> $iterator */
         $iterator = new MapIterator($this, $mapFn);
+        return $iterator;
+    }
+
+    /**
+     * @template TMappedValue
+     * @param Closure(TValue, TKey=): TMappedValue $mapFn
+     * @return MapIterator<TKey, TMappedValue, TValue>
+     * @phpstan-ignore-next-line we know its covariant because we only call the function on this iterator it's content
+     */
+    public function mapKey(Closure $mapFn): WrappedIterator
+    {
+        /** @var MapKeyIterator<TKey, TMappedValue, TValue> $iterator */
+        $iterator = new MapKeyIterator($this, $mapFn);
         return $iterator;
     }
 
@@ -38,34 +54,86 @@ trait IteratorExtensions
      * @return Set<TValue>
      * @phpstan-ignore-next-line we know its covariant because we only call the function on this iterator it's content
      */
-    public function toSet(): Set {
+    public function toSet(): Set
+    {
         return Set::fromTraversable($this);
     }
-
 
     /**
      * @return Stack<TValue>
      * @phpstan-ignore-next-line we know its covariant because we only call the function on this iterator it's content
      */
-    public function toStack(): Stack {
+    public function toStack(): Stack
+    {
         return Stack::fromTraversable($this);
     }
 
-    public function count(): int {
+    /**
+     * @return Map<TKey, TValue>
+     */
+    public function toMap(): Map
+    {
+        return Map::fromTraversable($this);
+    }
+
+    /**
+     * @return array<TKey, TValue>
+     */
+    public function toArray(): array
+    {
+        return iterator_to_array($this);
+    }
+
+    /**
+     * @template TOutput
+     * @param TOutput $initialValue
+     * @param Closure(TOutput, TValue, TKey=): TOutput
+     * @return TOutput
+     */
+    public function reduce(mixed $initialValue, Closure $reduceFn): mixed
+    {
+        $value = $initialValue;
+        foreach ($this as $key => $value) {
+            $value = $reduceFn($initialValue, $value, $key);
+        }
+        return $value;
+    }
+
+    public function count(): int
+    {
         $length = 0;
-        foreach($this as $_){
-            $length ++;
+        foreach ($this as $_) {
+            $length++;
         }
         return $length;
+    }
+
+    /**
+     * @param string $glue
+     * @return string
+     */
+    public function join(string $glue): string
+    {
+        $result = '';
+        $isFirst = true;
+        foreach ($this as $value) {
+            if (!$isFirst) {
+                $result .= $glue;
+            }
+            $isFirst = false;
+            $result .= (string)$value;
+        }
+        return $result;
     }
 
     /**
      * @param callable(TValue, TKey=): bool $filterFn
      * @return bool
      */
-    public function all(callable $filterFn): bool {
-        foreach($this as $key => $value){
-            if(!$filterFn($value, $key)){
+    public function all(callable $filterFn): bool
+    {
+        foreach ($this as $key => $value) {
+            if (!$filterFn($value, $key)) {
                 return false;
             }
         }
@@ -76,12 +144,27 @@ trait IteratorExtensions
      * @param callable(TValue, TKey=): bool $filterFn
      * @return bool
      */
-    public function any(callable $filterFn): bool {
-        foreach($this as $key => $value){
-            if($filterFn($value, $key)){
+    public function any(callable $filterFn): bool
+    {
+        foreach ($this as $key => $value) {
+            if ($filterFn($value, $key)) {
                 return true;
             }
         }
         return false;
+    }
+
+    /**
+     * @param callable(TValue, TKey=): bool $filterFn
+     * @return TValue|null
+     */
+    public function first(callable $filterFn): ?bool
+    {
+        foreach ($this as $key => $value) {
+            if ($filterFn($value, $key)) {
+                return $value;
+            }
+        }
+        return null;
     }
 }
---
+++ src/Iterator/MapKeyIterator.php
@@ -0,0 +1,37 @@
+<?php
+declare(strict_types=1);
+
+namespace DataStructures\Iterator;
+
+use Closure;
+use Iterator;
+
+/**
+ * @template TKey
+ * @template-covariant TValue
+ * @template TMapFrom
+ * @extends WrappedIterator<TKey, TValue>
+ */
+class MapKeyIterator extends WrappedIterator
+{
+    /**
+     * @param Iterator<TMapFrom, TValue> $iterator
+     * @param Closure(TValue, TMapFrom): TKey $mapFn
+     */
+    public function __construct(Iterator $iterator, private readonly Closure $mapFn)
+    {
+        /** @phpstan-ignore-next-line even though it's not the right type we fix it by overwriting current() */
+        parent::__construct($iterator);
+    }
+
+    /**
+     * @return TKey
+     */
+    public function key(): mixed
+    {
+        $mapFn = $this->mapFn;
+        /** @var TMapFrom $value */
+        $value = parent::key();
+        return $mapFn($value, parent::key());
+    }
+}
