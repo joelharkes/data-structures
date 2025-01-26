44566217882714265d5c6aa0a483e2b6dd617fdd
Joel Harkes <johannes.harkes@frontify.com>
2025-01-26 13:45:35 +0100

    tryout benchmarking

---
+++ benchmarks/IteratorBench.php
@@ -0,0 +1,37 @@
+<?php declare(strict_types=1);
+
+/* (c) Copyright Frontify Ltd., all rights reserved. */
+
+namespace benchmarks;
+
+use DataStructures\Iterator\WrappedIterator;
+
+class IteratorBench
+{
+    /**
+     * @Revs(1000)
+     * @Iterations(2)
+     */
+    public function benchIterator()
+    {
+        $data = range(1, 1000);
+        $iterator = new WrappedIterator(new \ArrayIterator($data));
+        $result = $iterator->skip(5)->take(900)
+            ->filter(fn($v) => $v % 2 === 0)
+            ->map(fn($v) => $v * 2)
+            ->toArray();
+    }
+
+    /**
+     * @Revs(1000)
+     * @Iterations(2)
+     */
+    public function benchStandard()
+    {
+        $data = range(1, 1000);
+        $slice = array_slice($data, 5, 900);
+        $filtered = array_filter($slice, fn($v) => $v % 2 === 0);
+        $mapped = array_map(fn($v) => $v * 2, $filtered);
+        $result = array_values($mapped);
+    }
+}
